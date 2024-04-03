0df0cb566ce79673f4a194334a8071b622646174
Julian Alconcher <alconcherjulian@gmail.com>
2024-04-03 20:23:41 -0300

    inicio repositorio

---
+++ pandasToOpenCv.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import cv2
+
+# Cargar datos desde el archivo CSV en un DataFrame
+file_path = "pose_data.csv"  
+df = pd.read_csv(file_path)
+
+# Abre el video para lectura
+cap = cv2.VideoCapture("FisicaI/Ejercicios/Videos/fran-bicep-3.MOV") # Reemplaza 'ruta_del_video.mp4' con la ruta de tu video
+
+window_name = 'Skeleton'
+
+# Iterar sobre cada frame en el video
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # Obtener las coordenadas de las articulaciones del frame actual
+    shoulder_points = [(int(row['Shoulder_X']), int(row['Shoulder_Y'])) for index, row in df[df['Frame'] == cap.get(cv2.CAP_PROP_POS_FRAMES)].iterrows()]
+    elbow_points = [(int(row['Elbow_X']), int(row['Elbow_Y'])) for index, row in df[df['Frame'] == cap.get(cv2.CAP_PROP_POS_FRAMES)].iterrows()]
+    wrist_points = [(int(row['Wrist_X']), int(row['Wrist_Y'])) for index, row in df[df['Frame'] == cap.get(cv2.CAP_PROP_POS_FRAMES)].iterrows()]
+
+    # Dibujar círculos para cada articulación en el frame actual
+    for shoulder_point, elbow_point, wrist_point in zip(shoulder_points, elbow_points, wrist_points):
+        cv2.circle(frame, shoulder_point, 5, (0, 255, 0), -1)  # Dibujar círculo para hombro
+        cv2.circle(frame, elbow_point, 5, (0, 0, 255), -1)     # Dibujar círculo para codo
+        cv2.circle(frame, wrist_point, 5, (255, 0, 0), -1)     # Dibujar círculo para muñeca
+
+    # Mostrar el cuadro con los puntos dibujados
+    cv2.imshow(window_name, frame)
+
+    # Salir si se presiona 'q'
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+# Liberar el video y cerrar todas las ventanas
+cap.release()
+cv2.destroyAllWindows()
---
+++ pruebaBicepMediaPipePandas.py
@@ -0,0 +1,58 @@
+import cv2
+import mediapipe as mp
+import pandas as pd
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+# Inicializamos las columnas del dataframe
+columns = ['Frame', 'Shoulder_X', 'Shoulder_Y', 'Elbow_X', 'Elbow_Y', 'Wrist_X', 'Wrist_Y']
+df = pd.DataFrame(columns=columns)
+
+cap = cv2.VideoCapture("FisicaI/Ejercicios/Videos/fran-bicep-3.MOV")
+
+# Procesamos cada frame, y extraemos landmarks
+frame_number = 0
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = pose.process(image)
+
+    # Extraer landmarks 
+    if results.pose_landmarks:
+        shoulder = None
+        elbow = None
+        wrist = None
+        for id, landmark in enumerate(results.pose_landmarks.landmark):
+            if id == mp_pose.PoseLandmark.LEFT_SHOULDER.value:
+                shoulder = (int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0]))
+            elif id == mp_pose.PoseLandmark.LEFT_ELBOW.value:
+                elbow = (int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0]))
+            elif id == mp_pose.PoseLandmark.LEFT_WRIST.value:
+                wrist = (int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0]))
+
+        if shoulder and elbow and wrist:
+            df = df._append({'Frame': frame_number,
+                            'Shoulder_X': shoulder[0], 'Shoulder_Y': shoulder[1],
+                            'Elbow_X': elbow[0], 'Elbow_Y': elbow[1],
+                            'Wrist_X': wrist[0], 'Wrist_Y': wrist[1]}, ignore_index=True)
+            frame_number += 1
+
+    # Display
+    cv2.imshow('MediaPipe Pose Detection', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+# Print the DataFrame
+# Save the DataFrame to a CSV file
+df.to_csv('pose_data.csv', index=False)
+
+print(df)
