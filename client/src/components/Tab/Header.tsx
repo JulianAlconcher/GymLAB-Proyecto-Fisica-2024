d7404cdfe41c8b24512a2a1cf760d4d004dac8ae
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-05-21 16:19:48 -0300

    Add Informacion y rutas

--- client/src/components/Tab/Header.tsx
+++ client/src/components/Tab/Header.tsx
@@ -10,15 +10,16 @@ function Header(){
         <header className="bg-black text-white py-4 px-6">
         <div className="container mx-auto flex justify-between items-center">
           <div className= "flex items-center">
-            <img src={pesa} alt="Gym Icon" className="h-8 mr-2" />
+            <a href="/">
+              <img src={pesa} alt="Gym Icon" className="h-8 mr-2" />
+            </a>
             <h1 className="text-2xl font-bold font-inter"onClick={() => navigate('/')}style={{ cursor: 'pointer' }}>
               GymLAB
             </h1>
           </div>
           <nav className="flex space-x-4">
             <button className="hover:text-gray-300" onClick={() => navigate("/guide")}>Guía de uso</button>
-            <button className="hover:text-gray-300" onClick={() => navigate("/about")}>Como Funciona</button>
-           <button className="hover:text-gray-300" onClick={() => navigate("/contact")}>Contacto</button>
+            <button className="hover:text-gray-300" onClick={() => navigate("/about")}>Informacion</button>
           </nav>
         </div>
       </header>
--- client/src/components/Tab/Pages/AboutPage.tsx
+++ client/src/components/Tab/Pages/AboutPage.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
 const AboutPage: React.FC = () => {
-    return <h1>About Page</h1>;
+     return (
+      <div className="font-sans p-6 max-w-5xl mx-auto bg-gray-100 rounded-lg shadow-md">
+        <h1 className="text-3xl text-center text-gray-800 mb-6">¿Sabías que? Cuando vas al gimnasio estás trabajando con física...</h1>
+        <section className="mb-6">
+          <p className="text-gray-600">
+          Cuando ingresas al gimnasio o te embarcas en una rutina de ejercicios , no solo estás desafiando tus músculos y tu sistema cardiovascular; También te involucrarás con los principios fundamentales de la física. Las leyes del movimiento de Sir Isaac Newton, que describen la relación entre un cuerpo y las fuerzas que actúan sobre él, desempeñan un papel importante a la hora de dar forma a su experiencia de ejercicio. En este blog, exploraremos cómo las leyes de Newton afectan su entrenamiento y brindaremos información sobre cómo optimizar su régimen de ejercicios.
+            </p>
+        </section>
+        
+        <section className="mb-6">
+          <h2 className="text-2xl text-gray-700 mb-2">Primera Ley de Newton</h2>
+          <p className="text-gray-600">
+            La primera ley de Newton, también conocida como la ley de la inercia, establece que un objeto en reposo permanecerá en reposo y un objeto en movimiento permanecerá en movimiento a menos que actúe sobre él una fuerza externa.
+          </p>
+        </section>
+        <section className="mb-6">
+          <h2 className="text-2xl text-gray-700 mb-2">Segunda Ley de Newton</h2>
+          <p className="text-gray-600">
+            La segunda ley de Newton establece que la fuerza neta que actúa sobre un objeto es igual a la masa del objeto multiplicada por su aceleración (F = ma).
+          </p>
+        </section>
+        <section>
+          <h2 className="text-2xl text-gray-700 mb-2">Tercera Ley de Newton</h2>
+          <p className="text-gray-600">
+            La tercera ley de Newton establece que para cada acción, hay una reacción igual y opuesta.
+          </p>
+        </section>
+      </div>
+    );
   };
 
   
