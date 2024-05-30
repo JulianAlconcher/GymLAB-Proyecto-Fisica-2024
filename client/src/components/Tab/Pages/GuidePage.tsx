29701bbd4707ff1864b050b969acac708def8a9f
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-05-30 16:01:47 -0300

    Agregar Guia (falta terminar)

--- client/src/components/Tab/Pages/GuidePage.tsx
+++ client/src/components/Tab/Pages/GuidePage.tsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React from 'react';
 
 const GuidePage: React.FC = () => {
-    return <h1>Guide Page</h1>;
-  };
+  return (
+    <div className="font-sans p-6 max-w-5xl mx-auto bg-gray-100 rounded-lg shadow-md">
+      <h1 className="text-3xl text-center text-gray-800 mb-6">Guía para utilizar GymLAB</h1>
+      
+      <section className="mb-6">
+        <h2 className="text-2xl text-gray-700 mb-2">Paso 1: Grabación del Video</h2>
+        <p className="text-gray-600">
+          Para utilizar nuestra aplicacion debes tener grabado un video del ejercicio que se desea analizar.
+          El video debe estar grabado a una distancia de 2 metros de distancia entre la camara y la persona que realiza el ejercicio.
+        </p>
+        <p className="text-gray-600 font-bold"> Imagen Ilustrativa:</p>
+        <img src="/DistanciaFisica.png" alt="Subir video" width="60%" className="h-auto"/>
+        <p className="text-gray-600 "> 
+        Tambien recomendamos que el video este bien claro y que el sitio de grabacion disponga de buena iluminacion para asegurar una mejor calidad</p>
+      </section>
+      
+      <section className="mb-6">
+        <h2 className="text-2xl text-gray-700 mb-2">Paso 2: Subir un video de tu ejercicio</h2>
+        <p className="text-gray-600">
+          Una vez que ya hayas grabado tu video realizando el ejercicio, deberas subirlo a la aplicacion.
+          </p>
+        <p className="text-gray-600 mb-4">
+          Pero previo a la subida del video se deben completar un formulario para la obtencion de datos.
+        </p>
+        <img src="/FormularioGuia.png" alt="Completar Formulario" width="50%" className="h-auto mb-4"/>
+        <p className="text-gray-600">
+          <b>Peso:</b> se debe indicar el peso actual de la persona que esta realizando el ejercicio.
+        </p>
+        <p className="text-gray-600">
+         <b> Altura:</b> se debe indicar la altura actual de la persona que esta realizando el ejercicio.
+        </p>
+        <p className="text-gray-600"> Luego se debe seleccionar el <b>genero</b> de la persona y su <b>nivel</b> en el gimnasio</p>
+        <p className="text-gray-600 mb-4"> Una vez completados estos datos se puede proceder a subir el video (El formato puede ser <b>MP4 o MOV </b>)</p>
+        <img src="/FormularioGuia2.png" alt="Subir video" width="50%" className="h-auto mb-4"/>
 
-  
-  export default GuidePage;
+      </section>
+      
+      <section className="mb-6">
+        <h2 className="text-2xl text-gray-700 mb-2">Paso 3: Analizar el video</h2>
+        <p className="text-gray-600">
+          Una vez que el video se haya subido, la aplicación analizará el ejercicio utilizando la Fisica y devolverá un resumen detallado. 
+        </p>
+        <p className="text-gray-600 mb-4">
+          Y podra observar el video con todos sus datos correspondientes y sus graficos como el siguiente: 
+        </p>
+        <img src="/MuestraGraficos.png" alt="Análisis de video" width="75%" className="h-auto mb-4"/>
+      </section>
+      
+      <section className="mb-6">
+        <h2 className="text-2xl text-gray-700 mb-2">Paso 4: Descarga del resumen</h2>
+        <p className="text-gray-600">
+          Después del análisis, podrás ver un resumen detallado de tu ejercicio. Esto incluirá estadísticas clave como 
+          velocidad, aceleración, fuerza y energía. Utiliza esta información para mejorar tu técnica y optimizar tu rendimiento.
+        </p>
+        <img src="/summary_example.png" alt="Resumen de ejercicio" width="75%" className="h-auto mb-4"/>
+      </section>
+    </div>
+  );
+};
+
+export default GuidePage;
