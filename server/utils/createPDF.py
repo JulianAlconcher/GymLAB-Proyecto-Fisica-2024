815ca8a18ab9e80de84bf72f97b6bf14eec8b339
Julian Alconcher <alconcherjulian@gmail.com>
2024-06-20 12:19:49 -0300

    Fix Pdf creation

--- server/utils/createPDF.py
+++ server/utils/createPDF.py
@@ -21,16 +21,18 @@ def create_pdf(weight=80, genre="Masculino", height=1.70, training_level=1, dist
         pdf.add_page()
 
         # Encabezado del PDF
-        pdf.set_font('Arial', 'B', 12)
+        pdf.set_font('Arial', 'B', 16)
         pdf.cell(0, 10, 'Reporte de Análisis de Fuerza', 0, 1, 'C')
+        pdf.set_font('Arial', 'B', 18)
+        pdf.cell(0, 10, 'GymLAB', 0, 1, 'C')
         pdf.ln(10)
 
         # Obtener el nivel de entrenamiento
         nivel_entrenamiento = getExperience(training_level)
 
         # Agregar los datos al PDF
-        pdf.set_font('Arial', 'B', 16)
-        pdf.cell(0, 10, 'Datos del análisis', 0, 1, 'C')
+        pdf.set_font('Arial', 'B', 14)
+        pdf.cell(0, 10, 'Datos generales del análisis', 0, 1, 'C')
         pdf.ln(10)
 
         pdf.set_font('Arial', '', 12)
@@ -40,11 +42,18 @@ def create_pdf(weight=80, genre="Masculino", height=1.70, training_level=1, dist
         pdf.cell(0, 10, f'Nivel de entrenamiento: {nivel_entrenamiento}', 0, 1)
         pdf.cell(0, 10, f'Distancia del antebrazo: {distance_forearm} cm', 0, 1)
         pdf.cell(0, 10, f'Masa de la mancuerna: {mass_weight} kg', 0, 1)
+        
+        last_reps = df["Reps"].astype(int).iloc[-1]
+
+        # Agregar el último entero al PDF
+        pdf.set_font('Arial', '', 12)
+        pdf.cell(0, 10, f'Cantidad de repeticiones realizadas: {last_reps}', 0, 1)
         pdf.ln(10)
 
         # Obtener la fuerza máxima y promedio
         max_force = df["max_fuerza_bicep"].iloc[0].round(2)
         avg_force = df["average_fuerza_bicep"].iloc[0].round(2)
+        
 
         # Agregar los datos de fuerza al PDF
         pdf.set_font('Arial', 'B', 14)
@@ -55,6 +64,23 @@ def create_pdf(weight=80, genre="Masculino", height=1.70, training_level=1, dist
         pdf.cell(0, 10, f'Fuerza máxima alcanzada: {max_force} N', 0, 1)
         pdf.cell(0, 10, f'Fuerza promedio: {avg_force} N', 0, 1)
         pdf.ln(10)
+        
+        # Agregar los datos de fuerza al PDF
+        pdf.set_font('Arial', 'B', 14)
+        pdf.cell(0, 10, 'Resultados de Energia', 0, 1, 'C')
+        pdf.ln(10)
+        
+        pdf.set_font('Arial', '', 12)
+        max_energy_potencial = df["energia_potencial"].max().round(2)
+        max_energy_cinetica = df["energia_cinetica"].max().round(2)
+        max_energy_mecanica = df["energia_mecanica"].max().round(2)
+
+        pdf.cell(0, 10, f'Mayor energía potencial: {max_energy_potencial}', 0, 1)
+        pdf.cell(0, 10, f'Mayor energía cinetica: {max_energy_cinetica}', 0, 1)
+        pdf.cell(0, 10, f'Mayor energía mecanica: {max_energy_mecanica}', 0, 1)
+        
+        pdf.ln(15)     
+        
 
         # Agregar las imágenes al PDF
         image_folder = "static"
@@ -62,22 +88,17 @@ def create_pdf(weight=80, genre="Masculino", height=1.70, training_level=1, dist
             print(f"Directorio {image_folder} no encontrado.")
             return False
 
-        pdf.set_font('Arial', 'B', 14)
-        pdf.cell(0, 10, 'Imágenes del análisis', 0, 1, 'C')
+
         pdf.ln(10)
 
         for filename in os.listdir(image_folder):
             if filename.endswith('.png'):
                 try:
-                    pdf.image(os.path.join(image_folder, filename), x=10, y=None, w=190, h=100)
-                    pdf.ln(20)  # Ajusta la separación entre imágenes según sea necesario
+                    pdf.image(os.path.join(image_folder, filename), x=30, y=None, w=150, h=80)
+                    pdf.ln(12)  # Ajusta la separación entre imágenes según sea necesario
                 except Exception as e:
                     print(f"No se pudo agregar la imagen {filename}: {e}")
 
-        # Pie de página
-        pdf.set_y(-15)
-        pdf.set_font('Arial', 'I', 8)
-        pdf.cell(0, 10, f'Página {pdf.page_no()}', 0, 0, 'C')
 
         # Guardar el PDF
         output_file = "analisis.pdf"
